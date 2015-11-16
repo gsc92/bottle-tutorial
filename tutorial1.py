31a3f1ce0a2199f4a5a14403196e7b07eec751d8
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-16 15:21:06 +0200

    Hello World

--- tutorial1.py
+++ tutorial1.py
@@ -1,1 +1,9 @@
-print("Tutorial 1")
+from bottle import Bottle, run
+
+app = Bottle()
+
+@app.route('/o')
+def hello():
+    return "Hello guest, How Are You?"
+
+run(app, host='localhost', port=8080)
