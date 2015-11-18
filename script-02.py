0eaa28be9f68f7c3d8cfa51ffdcd3d0419e83360
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-18 04:32:21 +0200

    Added script-02

--- script-02.py
+++ script-02.py
@@ -1,13 +1,9 @@
-import bottle
-bottle.TEMPLATE_PATH = ['template']
+from bottle import error,run,app
 
-from bottle import Bottle, run, template
 
-app = Bottle()
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
 
-@app.route('/')
-@app.route('/<name:re:[a-z]+>')
-def index(name='world'):
-    return template('script-01', name=name)
 
 run(app, host='localhost', port=8080)
