a4df6de7d36024353436b52d74999e7ebc4a2036
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-19 00:47:17 +0200

    upper latters

--- script-02.py
+++ script-02.py
@@ -2,6 +2,8 @@ from bottle import error,run,route
 
 
 @route('/wiki/<pagename>')            # matches /wiki/Learning_Python
+@route('<pagename:re:[A-Z]+>')
+
 def show_wiki_page(pagename):
     print('[show-wiki-page]', pagename)
     return pagename
