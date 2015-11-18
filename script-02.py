4949db1ea6da5ce0124dd59726fad351c6ab5228
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-19 01:18:15 +0200

    second route is not working

--- script-02.py
+++ script-02.py
@@ -1,9 +1,22 @@
-from bottle import request,run
+from bottle import request,route,run,response,template
 
-r=request.get("https://github.com/", auth=('user', 'pass'))
 
-print(r.status_code)
-print(r.headers['content-type'])
+@route('/login')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        response.set_cookie("account", username, secret='some-secret-key')
+        return template("<p>Welcome {{name}}! You are now logged in.</p>", name=username)
+    else:
+        return "<p>Login failed.</p>"
 
+@route('/restricted')
+def restricted_area():
+    username = request.get_cookie("account", secret='some-secret-key')
+    if username:
+        return template("Hello {{name}}. Welcome back.", name=username)
+    else:
+        return "You are not logged in. Access denied."
 
 run( host='localhost', port=8080)
