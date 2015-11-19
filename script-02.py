1bb43617178ea7976ddce44d2b89437250ecfcad
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-19 12:39:54 +0200

    in browser 'file does not exist'

--- script-02.py
+++ script-02.py
@@ -1,9 +1,9 @@
-from bottle import static_file,route, run
+from bottle import request,route,run,static_file
 
-from bottle import static_file
+
 @route('/static/<filename>')
 def server_static(filename):
-    return static_file(filename, root='C:\\Users\GOKCE\Desktop\1.jpg')
+    return static_file(filename, root='C://Users/GOKCE/Desktop/1.jpg')
 
 
-run(host='localhost', port=8082)
+run(host='localhost', port=8080)
