032890cd7aa7ecda6a048be13ab681838bfa059d
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-23 03:46:33 +0200

    it work

--- scritp-03.py
+++ scritp-03.py
@@ -1,8 +1,8 @@
-from bottle import static_file, route, run
+from bottle import redirect, route, run
 
-from bottle import route, abort
-@route('/restricted')
-def restricted():
-    abort(401, "Sorry, access denied.")
+
+@route('/wrong/url')
+def wrong():
+    redirect("/right/url")
 
 run(host='localhost', port=8081)
