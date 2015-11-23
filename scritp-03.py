e42d10f274478cc66c8eb33368df9f1cf25985be
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-23 05:11:35 +0200

    it works

--- scritp-03.py
+++ scritp-03.py
@@ -1,8 +1,11 @@
-from bottle import redirect, route, run
+from bottle import request, route, run, response
 
-
-@route('/wrong/url')
-def wrong():
-    redirect("/right/url")
+@route('/hello')
+def hello_again():
+    if request.get_cookie("visited"):
+        return "Welcome back! Nice to see you again"
+    else:
+        response.set_cookie("visited", "yes")
+        return "Hello there! Nice to meet you"
 
 run(host='localhost', port=8088)
