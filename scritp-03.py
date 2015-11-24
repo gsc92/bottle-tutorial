1f3bd69bfaaef8ad4599f5da59cdca46da96c567
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-24 14:50:33 +0200

    fixed static-file routes

--- scritp-03.py
+++ scritp-03.py
@@ -1,12 +1,12 @@
 from bottle import static_file, route, run
 
 from bottle import static_file
-@route('/images/<filename:re:.*\.png>')
+@route('/images/<filename:re:.*\.jpg>')
 def send_image(filename):
-    return static_file(filename, root='/path/to/image/files', mimetype='image/png')
+    return static_file(filename, root='./static/', mimetype='image/jpg')
 
 @route('/static/<filename:path>')
 def send_static(filename):
-    return static_file(filename, root='/path/to/static/files')
+    return static_file(filename, root='./static/')
 
 run(host='localhost', port=8081)
