2c457dfab1252211f4dd61cb591c6550260d030b
Gökçe Sarıçiyil <goekce.sariciyil@blackhan.com>
2015-11-25 03:42:30 +0200

    "get and post methods" is working.

--- script-04.py
+++ script-04.py
@@ -1,10 +1,22 @@
 from bottle import route, request, run
 
-@route('/is_ajax')
-def is_ajax():
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return 'This is an AJAX request'
+@route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if login(username, password):
+        return "<p>Your login information was correct.</p>"
     else:
-        return 'This is a normal request'
+        return "<p>Login failed.</p>"
 
 run(host='localhost', port=8081)
